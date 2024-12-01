ac135a0ceadc7446379e2891a70f38b4d7b8eaae
CmD0 <elouan112@gmail.com>
2024-12-01 16:43:01 -0500

    day 1, part 1

---
+++ 1/src/Main.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+
+public class Main {
+    public static void main(String... args) {
+        try {
+            File input = new File("1/input");
+            Scanner scan = new Scanner(input);
+            int[] arr1 = new int[1000];
+            int[] arr2 = new int[1000];
+            for (int i = 0; i < 1000; ++i) {
+                arr1[i] = scan.nextInt();
+                arr2[i] = scan.nextInt();
+            }
+
+            int total = 0;
+            Arrays.sort(arr1);
+            Arrays.sort(arr2);
+            for (int i = 0; i < arr1.length; ++i) {
+                int distance = (arr1[i] <= arr2[i]) ? arr2[i] - arr1[i] : arr1[i] - arr2[i];
+                total += distance;
+            }
+            System.out.println(total);
+        } catch (FileNotFoundException exception) {
+            System.out.println("input file missing;");
+        }
+    }
+}
